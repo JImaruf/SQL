a331dc15e8741e884134592f70b7c1255dbccd2e
jahidul.maruf <jahidul.maruf@efficientsoftwaresolutions.com>
2026-06-03 19:37:05 +0600

    string function

--- Database/2_SQL_Operators/Like.sql
+++ Database/2_SQL_Operators/Like.sql
@@ -1,15 +1,16 @@
-SELECT p.product_name 
+
+SELECT p.product_name
 From production.products p
 Where p.product_name Like 'e%';
 
-SELECT p.product_name 
+SELECT p.product_name
 From production.products p
 Where p.product_name Like 'electra t%';
 
-SELECT p.product_name 
+SELECT p.product_name
 From production.products p
 Where p.product_name Like '%on%';
 
-SELECT p.product_name 
+SELECT p.product_name
 From production.products p
 Where p.product_name Like '%_on_%';
---
+++ Database/5_String_Function/String Function.sql
@@ -0,0 +1,17 @@
+SELECT CONCAT(product_name, ' - (', model_year,')') AS ProductInfo
+FROM production.products;
+
+SELECT SUBSTRING('Maruf Islam', 2, 7) as substring_value;
+
+SELECT LEFT('Bangladesh', 4) AS left_side;
+
+SELECT RIGHT('Bangladesh', 4) as right_side;
+
+SELECT LEN('Bangladesh') as lenght_of_string;
+
+SELECT TRIM('   Maruf   ');
+
+SELECT REPLACE('I love Java', 'Java', 'SQL');
+
+SELECT value
+FROM STRING_SPLIT('Apple,Banana,Mango', ',');
